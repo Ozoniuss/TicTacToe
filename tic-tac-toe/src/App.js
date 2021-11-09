a3e450f090eb26d6df7faa7e88b38d55e255e52c
Ozoniuss <alexbledea_2000@yahoo.com>
2021-11-09 22:50:24 +0200

    the gameplay is functional

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -14,19 +14,75 @@ function App() {
       0,0,0 
     ]);
 
+  const [hasWonPlayer, setHasWonPlayer] = useState(false);
+
+  const [turn, setTurn] = useState(1);
+
+  const incrementTurn = () => {setTurn(turn+1)};
+
+  const isDraw = () => {return turn == 10};
+
   const changeSquareValue = (location) => {
+    console.log(location)
     let newValues = values.slice();
     newValues[location] = currentPlayer
     setValues(newValues);
+
+    if (checkWinner(currentPlayer, newValues)){
+      setHasWonPlayer(true);
+    }
+
     setCurrentPlayer(currentPlayer === 1 ? 2 : 1);
+    incrementTurn();
+
   }
 
-  return (
-    <>
-      <h4>Current player is {currentPlayer}</h4>
-      <Board values={values} squareChanged={changeSquareValue}></Board>
-    </>
-  );
+  const checkWinner = (player, values) => {
+    const winningPossibilities = [
+      [0,1,2], [3,4,5], [6,7,8],
+      [0,3,6], [1,4,7], [2,5,8],
+      [0,4,8], [2,4,6]
+    ];
+
+    for (const combination of winningPossibilities){
+        if (values[combination[0]] == player &&
+            values[combination[1]] == player &&
+            values[combination[2]] == player)
+            {
+              return true;
+            }
+      }
+    return false;
+  }
+
+  if (hasWonPlayer){
+    return (
+      <>
+        <h4>Current player is {currentPlayer}</h4>
+        <h4>Congrats</h4>
+      </>
+    );
+  }
+
+  else if (!isDraw()){
+    return (
+      <>
+        <h4>Current player is {currentPlayer}</h4>
+        <Board values={values} squareChanged={changeSquareValue}></Board>
+      </>
+    );
+  }
+
+  else
+  {
+    return(
+      <>
+        <h4>Current player is {currentPlayer}</h4>
+        <h4>Draw</h4>
+      </>
+    );
+  }
+
 }
 
 export default App;
